5dd7745b52c30fe2d79ff289245a4826548cd2a7
Leonardo Taccari <iamleot@gmail.com>
2016-06-01 16:19:03 +0200

    bsd.debugdata.mk: add a first stub that just contains a lot of TODO
    At the moment it just contain documentation useful for the `help' target that
    will serve to keep in mind all the variables that can be used.

---
+++ mk/bsd.debugdata.mk
@@ -0,0 +1,20 @@
+#	$NetBSD$
+#
+# This Makefile fragment is included by bsd.pkg.mk and implements the
+# logic needed to strip debug symbols off the program/libraries.
+#
+# User-settable variables:
+#
+# PKG_DEBUGDATA: TODO: document me
+#
+# PKG_DEBUGLEVEL: TODO: document me
+#
+# Package-settable variables:
+#
+# DEBUGDATA_FILES: TODO: document me
+#
+# See also:
+#	INSTALL_UNSTRIPPED	
+#
+# Keywords: debug
+#
