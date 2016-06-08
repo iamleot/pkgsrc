ea83b561f4e1edf3eb339cf49d25bbc65e25c97e
Leonardo Taccari <iamleot@gmail.com>
2016-06-08 11:54:43 +0200

    bsd.debugdata.mk: delete XXX notes (containing brainstorming idea)
    All of them were addressed and now the rudimentary logic is implemented.

--- mk/bsd.debugdata.mk
+++ mk/bsd.debugdata.mk
@@ -26,19 +26,6 @@
 # Keywords: debug
 #
 
-#
-# XXX: various notes
-#
-# o INSTALL_UNSTRIPPED will be probably needed to be defined (see
-#   _INSTALL_UNSTRIPPED in bsd.pkg.mk).
-# o For all the logic we will probably need - at least for the moment - to
-#   inject post-install(s) ad-hoc targets that will run `objcopy' to strip the
-#   debug symbols off the programs/libraries. A possible place to look is
-#   pax.mk.
-# o GENERATE_PLIST should be used to dynamically generate the PLIST
-#   corresponding to the DEBUGDATA_FILES. For an example on how it is
-#   used please see lang/perl5/packlist.mk.
-
 .if !defined(BSD_DEBUGDATA_MK)
 BSD_DEBUGDATA_MK=	# defined
 
