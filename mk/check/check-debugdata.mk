e10fd5e6ddf965d085363059f0a31f9ef226fbb9
Leonardo Taccari <iamleot@gmail.com>
2016-06-10 19:20:31 +0200

    check-debugdata.mk: add a sketch version to check object files and .debug files
    Like most other check this is triggered only for PKG_DEVELOPERs and if
    PKG_DEBUGDATA is yes.
    Several part of the code was stolen from check-stripped.mk and check-shlibs.mk
    (but it's much more similar to the former for the spirit).
    
    TODO: ATM non-${OBJ_FMT} files aren't properly handled, this is a bug and should
    TODO: be fixed ASAP!
    
    XXX: (note to myself) check if the code can be simplified. Unfortunately putting
    XXX: the logic in separate script (shell or AWK) is probably not easy because in
    XXX: this context using DELAYED_*_MSG is particularly handy.
    XXX: If this can be easily obtained or complicate the logic without any real
    XXX: gains it will be probably better to provide two separate targets.
    XXX: One to just check programs/libraries. Another one to check for the
    XXX: `.debug*' sections in `.debug' files.
    
    XXX: Probably `_check-debugdata' and `*DEBUGDATA*' is abused in target and
    XXX: variable names. A better terminology to separate debugdata (`.debug' files)
    XXX: and program/libraries files is a possible improvement.

---
+++ mk/check/check-debugdata.mk
@@ -0,0 +1,97 @@
+# $NetBSD$
+#
+# This file verifies that all binaries and libraries used by the package have
+# correctly stripped the debug symbols and if they exists.
+#
+# User-settable variables:
+#
+# CHECK_DEBUGDATA
+#	Whether the check should be enabled or not.
+#
+#	Default value: "yes" for PKG_DEVELOPERs, "no" otherwise.
+#
+# Package-settable variables:
+#
+# CHECK_DEBUGDATA_SKIP
+#	A list of shell patterns (like man/*) that should be excluded
+#	from the check. Note that a * in a pattern also matches a slash
+#	in a pathname.
+#
+#	Default value: empty.
+#
+# CHECK_DEBUGDATA_SUPPORTED
+#	Whether the check should be enabled for this package or not.
+#
+#	Default value: yes
+#
+
+_VARGROUPS+=			check-debugdata
+_USERS_VARS.check-debugdata=	CHECK_DEBUGDATA
+_PKG_VARS.check-debugdata=	CHECK_DEBUGDATA_SUPPORTED
+
+.if !empty(PKG_DEBUGDATA:M[yY][eE][sS]) && ${PKG_DEVELOPER:Uno} != "no"
+CHECK_DEBUGDATA?=	yes
+.else
+CHECK_DEBUGDATA?=	no
+.endif
+CHECK_DEBUGDATA_SUPPORTED?=	yes
+CHECK_DEBUGDATA_SKIP?=		# none
+
+# All binaries and shared libraries.
+_CHECK_DEBUGDATA_ERE=	(bin/|sbin/|libexec/|\.(dylib|sl|so)$$|lib/lib.*\.(dylib|sl|so))
+
+_CHECK_DEBUGDATA_FILELIST_CMD?=	${SED} -e '/^@/d' -e '/\.debug$$/d' ${PLIST} |	\
+	(while read file; do							\
+		${TEST} -h "$$file" || ${ECHO} "$$file";			\
+	done)
+
+.if !empty(CHECK_DEBUGDATA:M[Yy][Ee][Ss]) && \
+    !empty(CHECK_DEBUGDATA_SUPPORTED:M[Yy][Ee][Ss])
+privileged-install-hook: _check-debugdata
+
+USE_TOOLS+=		objdump
+
+#
+# TODO: correctly handle non-${OBJ_FMT} files!
+# TODO: Probably we will need to parse objdump `File format not recognized'
+# TODO: output (unfortunately the errorcode is the same for non-existing
+# TODO: section).
+#
+_check-debugdata: error-check .PHONY
+	@${STEP_MSG} "Checking for missing debug data in ${PKGNAME}"
+	${RUN} rm -f ${ERROR_DIR}/${.TARGET}
+	${RUN}								\
+	cd ${DESTDIR:Q}${PREFIX:Q};					\
+	${_CHECK_DEBUGDATA_FILELIST_CMD} |				\
+	${EGREP} -h ${_CHECK_DEBUGDATA_ERE:Q} |				\
+	while read file; do						\
+		case "$$file" in					\
+		${CHECK_DEBUGDATA_SKIP:@p@${p}) continue ;;@}		\
+		*) ;;							\
+		esac;							\
+		dpfile=${DESTDIR}${PREFIX}/$$file;			\
+		if [ ! -r "$$dpfile" ]; then				\
+			${DELAYED_WARNING_MSG}				\
+			    "[check-debugdata.mk] File \"$$dpfile\" cannot be read."; \
+			continue;					\
+		fi;							\
+		if [ ! -f "$${dpfile}.debug" ]; then			\
+			${DELAYED_ERROR_MSG}				\
+			   "[check-debugdata.mk] File \"$${dpfile}.debug\" does not exists."; \
+			continue;					\
+		fi;							\
+		${TOOLS_PLATFORM.objdump} -hw -j '.gnu_debuglink' "$$dpfile" \
+		    1>/dev/null 2>/dev/null;				\
+		exitcode=$$?;						\
+		if [ $$exitcode -ne 0 ]; then				\
+			${DELAYED_ERROR_MSG}				\
+			   "[check-debugdata.mk] File \"$$dpfile\" does not have " \
+			   ".gnu_debuglink section.";			\
+			continue;					\
+		fi;							\
+	done
+
+.else
+_check-debugdata: error-check .PHONY
+	@${WARNING_MSG} "Skipping debug data check."
+.endif
