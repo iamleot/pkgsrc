f736d46c271272d74dbd467003726e32d5a31517
Leonardo Taccari <iamleot@gmail.com>
2016-08-20 18:50:48 +0200

    pkgformat.mk: SUBPACKAGES-ify PKG_FILELIST_CMD

--- mk/pkgformat/pkg/pkgformat.mk
+++ mk/pkgformat/pkg/pkgformat.mk
@@ -7,7 +7,14 @@
 # PKG_FILELIST_CMD outputs the list of files in the package based on
 # _DEPENDS_PLIST.
 #
+.if !empty(SUBPACKAGES)
+.  for _spkg_ in ${SUBPACKAGES}
+_DEPENDS_PLISTS+=	${_DEPENDS_PLIST.${_spkg_}}
+.  endfor
+PKG_FILELIST_CMD=	${SED} -e "/^@/d" -e "s|^|${PREFIX}/|" ${_DEPENDS_PLISTS}
+.else	# !SUBPACKAGES
 PKG_FILELIST_CMD=	${SED} -e "/^@/d" -e "s|^|${PREFIX}/|" ${_DEPENDS_PLIST}
+.endif	# SUBPACKAGES
 
 .include "depends.mk"
 .include "check.mk"
