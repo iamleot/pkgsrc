882853aa341796779ca08e60d1e0804ef1af0ab4
Leonardo Taccari <iamleot@gmail.com>
2017-06-20 12:29:21 +0200

    deinstall.mk: Adjust _PKG_ARGS_DEINSTALL per-subpackage
    This is needed per-spkg PKG_PRESERVE.

--- mk/pkgformat/pkg/deinstall.mk
+++ mk/pkgformat/pkg/deinstall.mk
@@ -1,5 +1,33 @@
 # $NetBSD: deinstall.mk,v 1.2 2013/05/09 23:37:26 riastradh Exp $
 
+.if !empty(SUBPACKAGES)
+.  for _spkg_ in ${SUBPACKAGES}
+# Set the appropriate flags to pass to pkg_delete(1) based on the value
+# of DEINSTALLDEPENDS (see pkgsrc/mk/install/deinstall.mk).
+#
+.if defined(DEINSTALLDEPENDS)
+.  if empty(DEINSTALLDEPENDS:M[nN][oO])
+.    if !empty(DEINSTALLDEPENDS:M[aA][lL][lL])
+_PKG_ARGS_DEINSTALL.${_spkg_}+=	-r	# for "update" target
+.    else
+_PKG_ARGS_DEINSTALL.${_spkg_}+=	-r -R	# for removing stuff in bulk builds
+.    endif
+.  endif
+.endif
+
+.if defined(PKG_VERBOSE)
+_PKG_ARGS_DEINSTALL.${_spkg_}+=	-v
+.endif
+
+.if defined(PKG_PRESERVE)
+.  if defined(_UPDATE_RUNNING) && !empty(_UPDATE_RUNNING:M[yY][eE][sS])
+_PKG_ARGS_DEINSTALL.${_spkg_}+=	-N -f -f	# update w/o removing any files
+
+MAKEFLAGS.su-deinstall+=	_UPDATE_RUNNING=YES
+.  endif
+.endif
+.  endfor
+.else	# !SUBPACKAGES
 # Set the appropriate flags to pass to pkg_delete(1) based on the value
 # of DEINSTALLDEPENDS (see pkgsrc/mk/install/deinstall.mk).
 #
@@ -17,7 +45,6 @@ _PKG_ARGS_DEINSTALL+=	-r -R	# for removing stuff in bulk builds
 _PKG_ARGS_DEINSTALL+=	-v
 .endif
 
-# TODOleot: spkg-ify PKG_PRESERVE!
 .if defined(PKG_PRESERVE)
 .  if defined(_UPDATE_RUNNING) && !empty(_UPDATE_RUNNING:M[yY][eE][sS])
 _PKG_ARGS_DEINSTALL+=	-N -f -f	# update w/o removing any files
@@ -25,6 +52,7 @@ _PKG_ARGS_DEINSTALL+=	-N -f -f	# update w/o removing any files
 MAKEFLAGS.su-deinstall+=	_UPDATE_RUNNING=YES
 .  endif
 .endif
+.endif	# SUBPACKAGES
 
 # _pkgformat-deinstall:
 #	Removes a package from the system.
@@ -45,8 +73,8 @@ _pkgformat-deinstall: .PHONY
 	"") found=`${_PKG_BEST_EXISTS} ${PKGWILDCARD.${_spkg_}:Q} || ${TRUE}`;;	\
 	esac;								\
 	if ${TEST} -n "$$found"; then					\
-		${ECHO} "Running ${PKG_DELETE} ${_PKG_ARGS_DEINSTALL} $$found"; \
-		${PKG_DELETE} ${_PKG_ARGS_DEINSTALL} "$$found" || ${TRUE} ; \
+		${ECHO} "Running ${PKG_DELETE} ${_PKG_ARGS_DEINSTALL.${_spkg_}} $$found"; \
+		${PKG_DELETE} ${_PKG_ARGS_DEINSTALL.${_spkg_}} "$$found" || ${TRUE} ; \
 	fi
 .  endfor
 .else	# !SUBPACKAGES
